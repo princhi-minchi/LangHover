18f0a67405d76ffa69ffa3e9d1f046f1c40ee924
Brahmasamudra Sateesh Raunak Sharma <raunaksharmabss@gmail.com>
2026-01-29 18:53:00 +0100

    Fixed "...CSS" error

--- vite.config.ts
+++ vite.config.ts
@@ -3,27 +3,29 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      plugins: [react()],
-      define: {
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      },
-      build: {
-        outDir: 'dist',
-        rollupOptions: {
-          output: {
-            // This forces the output files to have simple names
-            entryFileNames: `assets/[name].js`,
-            chunkFileNames: `assets/[name].js`,
-            assetFileNames: `assets/[name].[ext]`
-          }
+  const env = loadEnv(mode, '.', '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      }
+    },
+    base: '',
+    build: {
+      modulePreload: false,
+      outDir: 'dist',
+      rollupOptions: {
+        output: {
+          // This forces the output files to have simple names
+          entryFileNames: `assets/[name].js`,
+          chunkFileNames: `assets/[name].js`,
+          assetFileNames: `assets/[name].[ext]`
         }
       }
-    };
+    }
+  };
 });
